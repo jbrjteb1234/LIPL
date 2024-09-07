79695bd1aa2f0e87e262d1e762a7c2d0008b58c4
jbrjteb1234 <james@bright.me.uk>
2024-09-07 19:03:09 +0100

    updated N and A entries to be uint32_t correctly

--- ast/reducer.h
+++ ast/reducer.h
@@ -1,12 +1,12 @@
 #ifndef REDUCER
 #define REDUCER
 
-#define N -1
-#define A -2
+#define N 0x30000000
+#define A 0x40000000
 
 #define reduction_mask  0x20000000  //indicates that this is a reduction call
 #define reduction_return_state_shift_count 20
-#define R(X,R) ((reduction_mask | X) | R<<reduction_return_state_shift_count)
+#define R(reduction_rule,return_state) ((reduction_mask | reduction_rule) | return_state<<reduction_return_state_shift_count)
 
 #include "../utility/stack.h"
 #include "ast.h"
