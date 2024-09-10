6cfb73d5912ede285787898b83918513fa1e75ca
jbrjteb1234 <james@bright.me.uk>
2024-09-10 05:36:34 +0100

    Added none type to table types

--- ast/states.h
+++ ast/states.h
@@ -11,14 +11,13 @@
 #define WIDTH 10
 #define HEIGHT 10
 
-#define NONE -1
-
 //converts int (jump rule)
 #define jump_mask 0x10000000
 #define new_state_shift_count 20
 #define J(table_to_jump, new_state) ((jump_mask | (table_to_jump&0x000fffff)) | (new_state << new_state_shift_count))
 
 typedef enum {
+    NONE = -1,
     NUMBERS_TABLE = 0,
     DECL_TABLE = 1
 } table_type;
