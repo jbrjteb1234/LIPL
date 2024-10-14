a6dd33acd434ce8bb05432c8ea08ca15d5a2123f
jbrjteb1234 <james@bright.me.uk>
2024-10-14 19:44:07 +0100

    Added routines file for commonly used ast management routines

---
+++ ast/ast_utility/routines.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+
+#include "routines.h"
+
+uint32_t open_expression_parentheses(table_iterator* iterator, uint32_t new_state){
+    uint32_t open_bracket_state_marker = C;
+    uint32_t state_after_close = new_state & 0x000fffff;
+    uint32_t bracket_state = (new_state & 0x0ff00000) >> open_parentheses_state_shift_count;
+    new_state = bracket_state;
+    printf("Opening bracket via state: %d\n", bracket_state);
+    push(iterator->current->return_stack, &state_after_close, true);
+    push(iterator->current->return_stack, &open_bracket_state_marker, true);
+    return new_state;
+}
---
+++ ast/ast_utility/routines.h
@@ -0,0 +1,5 @@
+#include "ast.h"
+#include "../tables/tables.h"
+
+uint32_t open_expression_parentheses(table_iterator* iterator, uint32_t new_state);
+
