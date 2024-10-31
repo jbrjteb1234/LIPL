9a5a2cb4fe33578ae5415a78e486de5306f16703
jbrjteb1234 <james@bright.me.uk>
2024-10-31 03:02:08 +0000

    Removed parsing for EXPR {

--- src/ast/tables/expr_table.c
+++ src/ast/tables/expr_table.c
@@ -3,28 +3,28 @@ state_table* get_expr_table(void){
     //  R(0) = reduce binary operator (like 1+1 or 2*1)
     //  0 var      1 id    2 +-    3 */     4 eos   5 .     6 =     7 ,         8 (         9 )         10 COMP     11 {
     static state_table expr_table = {
-        {N,         N,      2,      3,      A,      8,      N,      13,         N,          C,          20,         N},            //STATE 0: var/expr
-        {N,         N,      2,      3,      A,      N,      10,     13,         SNA(18),    C,          20,         N},            //STATE 1: ID
+        {N,         N,      2,      3,      A,      8,      N,      13,         N,          C,          20,         N},             //STATE 0: var/expr
+        {N,         N,      2,      3,      A,      N,      10,     13,         SNA(18),    C,          20,         N},             //STATE 1: ID
 
         {4,         6,      N,      N,      N,      N,      N,      N,          O(17,4),    N,          N,          N},             //STATE 2: expr +-
         {5,         7,      N,      N,      N,      N,      N,      N,          O(17,5),    N,          N,          N},             //STATE 3: expr */
 
-        {N,         N,      R(0,0), S(3),   R(0,0), S(8),   N,      R(0,0),     N,          R(0,0),     R(0,0),     R(0,0)},        //STATE 4: expr +- var/expr
-        {N,         N,      R(0,0), R(0,0), R(0,0), S(8),   N,      R(0,0),     N,          R(0,0),     R(0,0),     R(0,0)},        //STATE 5: expr */ var/expr
-        {N,         N,      R(0,0), S(3),   R(0,0), N,      N,      N,          SNA(18),    R(0,0),     R(0,0),     R(0,0)},        //STATE 6: expr +- ID
-        {N,         N,      R(0,0), R(0,0), R(0,0), N,      N,      N,          SNA(18),    R(0,0),     R(0,0),     R(0,0)},        //STATE 7: expr */ ID
+        {N,         N,      R(0,0), S(3),   R(0,0), S(8),   N,      R(0,0),     N,          R(0,0),     R(0,0),     N},             //STATE 4: expr +- var/expr
+        {N,         N,      R(0,0), R(0,0), R(0,0), S(8),   N,      R(0,0),     N,          R(0,0),     R(0,0),     N},             //STATE 5: expr */ var/expr
+        {N,         N,      R(0,0), S(3),   R(0,0), N,      N,      N,          SNA(18),    R(0,0),     R(0,0),     N},             //STATE 6: expr +- ID
+        {N,         N,      R(0,0), R(0,0), R(0,0), N,      N,      N,          SNA(18),    R(0,0),     R(0,0),     N},             //STATE 7: expr */ ID
 
         {9,         N,      N,      N,      N,      N,      N,      N,          N,          N,          N,          N},             //STATE 8: var .
-        {N,         N,      R(0,0), R(0,0), R(0,0), N,      N,      R(0,0),     N,          R(0,0),     R(0,0),     R(0,0)},        //STATE 9: var . var
+        {N,         N,      R(0,0), R(0,0), R(0,0), N,      N,      R(0,0),     N,          R(0,0),     R(0,0),     N},             //STATE 9: var . var
 
         {9,         9,      N,      N,      N,      N,      N,      N,          O(17,9),    N,          N,          N},             //STATE 10: ID =
-        {N,         N,      S(2),   S(3),   R(0,1), S(8),   N,      R(0,1),     N,          R(0,1),     N,          R(0,1)},        //STATE 11: ID = var/expr
+        {N,         N,      S(2),   S(3),   R(0,1), S(8),   N,      R(0,1),     N,          R(0,1),     N,          N},             //STATE 11: ID = var/expr
         {N,         N,      S(2),   S(3),   R(0,1), S(8),   N,      R(0,1),     SNA(18),    R(0,1),     N,          N},             //STATE 12: ID = ID
 
         {14,        15,     N,      N,      N,      N,      N,      N,          N,          N,          N,          N},             //STATE 13: expr ,           [expr]
-        {N,         N,      S(2),   S(3),   R(1,16),S(8),   N,      R(1,16),    N,          R(1,16),    N,          R(1,16)},       //STATE 14: expr , var/expr     [expr expr]
-        {N,         N,      S(2),   S(3),   R(1,16),N,      S(10),  R(1,16),    SNA(18),    R(1,16),    N,          R(1,16)},       //STATE 15: expr , ID
-        {N,         N,      N,      N,      A,      N,      N,      13,         N,          C,          N,          OB},            //STATE 16: tuple
+        {N,         N,      S(2),   S(3),   R(1,16),S(8),   N,      R(1,16),    N,          R(1,16),    N,          N},             //STATE 14: expr , var/expr     [expr expr]
+        {N,         N,      S(2),   S(3),   R(1,16),N,      S(10),  R(1,16),    SNA(18),    R(1,16),    N,          N},             //STATE 15: expr , ID
+        {N,         N,      N,      N,      A,      N,      N,      13,         N,          C,          N,          N},             //STATE 16: tuple
 
 
         {0,         1,      N,      N,      N,      N,      N,      N,          O(17,0),    C,          N,          N},             //STATE 17: (
@@ -32,8 +32,8 @@ state_table* get_expr_table(void){
         {N,         N,      R(2,0), R(2,0), R(2,0), N,      N,      R(2,0),     N,          R(2,0),     R(2,0),     R(2,0)},        //STATE 19: FCALL (...)
 
         {21,        22,     N,      N,      N,      N,      N,      N,          O(17,22),   N,          N,          N},             //STATE 20: expr COMP
-        {N,         N,      S(2),   S(3),   R(0,0), S(8),   N,      N,          N,          R(0,0),     R(0,0),     R(0,0)},        //STATE 21: expr COMP var/expr
-        {N,         N,      S(2),   S(3),   R(0,0), N,      N,      N,          SNA(18),    R(0,0),     R(0,0),     R(0,0)},        //STATE 22: expr COMP id
+        {N,         N,      S(2),   S(3),   R(0,0), S(8),   N,      N,          N,          R(0,0),     R(0,0),     N},             //STATE 21: expr COMP var/expr
+        {N,         N,      S(2),   S(3),   R(0,0), N,      N,      N,          SNA(18),    R(0,0),     R(0,0),     N},             //STATE 22: expr COMP id
 
 
     };
